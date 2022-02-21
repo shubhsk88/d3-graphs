6428bed5c7412e25f3f5efd8c9ff2f196a039868
Shubham <shubham88singh@gmail.com>
2022-02-21 14:00:30 +0530

    Addiong the dataset in the parcel using diffferent method

--- index.js
+++ index.js
@@ -1,89 +1,52 @@
 import * as d3 from 'd3';
-async function drawBarChart() {
-  const data = await d3.json('./my_weather_data.json');
-  console.log(data);
-  const xAccessor = (d) => d.humidity;
-  const yAccessor = (d) => d.length;
+import dataset from './my_weather_data.json';
+
+function drawChart() {
+  // 1. Access data
+  console.log(dataset);
+
+  // 2. Create chart dimensions
+
   const width = 600;
-  const dms = {
-    width,
-    height: 300,
+  let dimensions = {
+    width: width,
+    height: width,
+    radius: width / 2,
     margin: {
-      top: 30,
-      left: 50,
-      right: 10,
-      bottom: 50,
+      top: 120,
+      right: 120,
+      bottom: 120,
+      left: 120,
     },
   };
-  dms.boundedWidth = dms.width - dms.margin.left - dms.margin.right;
-  dms.boundedHeight = dms.height - dms.margin.top - dms.margin.bottom;
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+  dimensions.boundedRadius =
+    dimensions.radius - (dimensions.margin.left + dimensions.margin.right) / 2;
+
+  // 3. Draw canvas
+
   const wrapper = d3
     .select('#wrapper')
     .append('svg')
-    .attr('width', dms.width)
-    .attr('height', dms.height)
-    .style('border', '1px solid grey');
-  const bounded = wrapper
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height);
+
+  const bounds = wrapper
     .append('g')
-    .style('transform', `translate(${dms.margin.left}px,${dms.margin.top}px)`);
+    .style(
+      'transform',
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    );
 
-  const xScale = d3
-    .scaleLinear()
-    .domain(d3.extent(data, xAccessor))
-    .range([0, dms.boundedWidth])
-    .nice();
+  // 4. Create scales
 
-  const binsGenerator = d3
-    .bin()
-    .domain(xScale.domain())
-    .value(xAccessor)
-    .thresholds(12);
+  // 5. Draw data
 
-  const bins = binsGenerator(data);
-  const yScale = d3
-    .scaleLinear()
-    .domain([0, d3.max(bins, yAccessor)])
-    .range([dms.boundedHeight, 0])
-    .nice();
+  // 6. Draw peripherals
 
-  const binsGroup = bounded.append('g');
-
-  const binGroups = binsGroup.selectAll('g').data(bins).join('g');
-  const barPadding = 1;
-  const barRects = binGroups
-    .append('rect')
-    .attr('x', (d) => xScale(d.x0) + barPadding / 2)
-    .attr('y', (d) => yScale(yAccessor(d)))
-    .attr('width', (d) =>
-      d3.max([0, -xScale(d.x0) + xScale(d.x1) - barPadding])
-    )
-    .attr('height', (d) => dms.boundedHeight - yScale(yAccessor(d)))
-    .attr('fill', 'cornflowerblue');
-
-  const barText = binGroups
-    .filter(yAccessor)
-    .append('text')
-    .attr('x', (d) => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
-    .attr('y', (d) => yScale(yAccessor(d)) - 5)
-    .text(yAccessor)
-    .style('text-anchor', 'middle')
-    .style('font-family', 'serif')
-    .style('font-size', '12px')
-    .style('fill', 'darkgrey');
-  const mean = d3.mean(data, xAccessor);
-  const meanLine = bounded
-    .append('line')
-    .attr('x1', xScale(mean))
-    .attr('x2', xScale(mean))
-    .attr('y1', -15)
-    .attr('y2', dms.boundedHeight)
-    .attr('stroke', 'maroon')
-    .style('stroke-dasharray', '2px 4px');
-
-  const meanText = bounded
-    .append('text')
-    .attr('x', xScale(mean))
-    .attr('y', -15);
+  // 7. Set up interactions
 }
-
-drawBarChart();
+drawChart();
